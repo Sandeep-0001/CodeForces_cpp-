66f9e5fe88ef3a5e19d9881ed72e5195f70ba441
Sandeep <mrsandeepg0001@gmail.com>
2025-10-22 23:08:34 +0530

    add question

---
+++ A_Soldier_and_Bananas.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int k,n,w;
+    cin>>k>>n>>w;
+    int total=0;
+    for(int i=1;i<=w;i++){
+        total+=k*i;
+    }
+    if(total>n){
+        cout<<total-n;
+    }else{
+        cout<<0;
+    }
+    return 0;
+}
